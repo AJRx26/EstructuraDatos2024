d25b94dd61eb98621efccd64c6b348c756cc93ff
AJRx26 <122428038+AJRx26@users.noreply.github.com>
2024-02-15 12:31:28 -0600

    Add files via upload

--- Codigos II - Ubun/serie.py
+++ Codigos II - Ubun/serie.py
@@ -1,12 +1,16 @@
 def serie(n):
-    if n == 1:
+    if n == 1: #n==1 or n==2:
         return 1
     elif n == 2:
         return 1
     else:
         a, b = 1, 1
+        #a=1
+        #b=1
         for x in range(3, n + 1):
             a, b = b, a + b
+            #a=b
+            #b=a+b
         return b
 
 n = int(input())
